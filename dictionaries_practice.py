4eaacabe2c540ddc14f74adaa558919fcd129708
Uliana Hrytsenko <uliana.hrytsenko.25@kse.org.ua>
2026-07-01 16:08:51 +0300

    completed second task (dictionaries)

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -20,9 +20,17 @@ EMPLOYEE_DB = {
 
 # Ваш код для завдань нижче:
 
+#
+# user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
+# user_profile["phone"] = +380991234567
+# user_profile["is_premium"] = False
+# user_profile.pop("discount")
+# print(user_profile)
 
-user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
-user_profile["phone"] = +380991234567
-user_profile["is_premium"] = False
-user_profile.pop("discount")
-print(user_profile)
+# grades = {"Alice": 95, "Bob": 80, "Charlie": 75, "Diana": 90}
+# a = list(grades.values())
+# average = sum(a) / len(a)
+# print(f"Average: {average:.2f}")
+
+# phone = "991234567"
+# print(f"+38{phone:010d}")...
