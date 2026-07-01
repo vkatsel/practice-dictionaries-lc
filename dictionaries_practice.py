41cdb1bce525016aace3e109db0d05fbeff24014
Mira <lytvyn.mira@gmail.com>
2026-07-01 16:17:51 +0300

    Complete third task (dictionares)

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -18,12 +18,4 @@ EMPLOYEE_DB = {
     }
 }
 
-# Ваш код для завдань нижче:
-
-user_profile = {"username": "cinema_fan",
-                "is_premium": True,
-                "discount": 15}
-user_profile["phone"] = "+380991234567"
-user_profile["is_premium"] = False
-user_profile.pop("discount")
-print(user_profile)
+print(f"Alise salary : {EMPLOYEE_DB['tech']['alice']['salary']}")
