36e9f82c06cabd99b16f84782c30e861d19d08b3
Inna-Tomashevska <inna.tomashevska.25@kse.org.ua>
2026-07-01 15:59:55 +0300

    Completed second task (dictionaries)

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -25,3 +25,7 @@ user_profile["phone"] = "+380991234567"
 user_profile["is_premium"] = False
 user_profile.pop("discount")
 print(user_profile)
+
+grades = {"Alice": 95, "Bob": 80, "Charlie": 75, "Diana": 90}
+average = sum(grades.values())/len(grades.values())
+print(average)
