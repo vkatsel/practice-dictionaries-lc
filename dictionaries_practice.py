7a7c1bb829d6c1bbf0f17d553ab427239648ab6d
alexandergerhert <oleksandr.herhert.25@kse.org.ua>
2026-07-01 16:22:07 +0300

    completed fourth task(dictionaries)

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -20,6 +20,10 @@ EMPLOYEE_DB = {
 
 print(EMPLOYEE_DB["tech"]["alice"]["salary"])
 
+EMPLOYEE_DB["sales"]["diana"]["salary"]=3500
+del EMPLOYEE_DB["tech"]["charlie"]
+print(EMPLOYEE_DB)
+
 # Ваш код для завдань нижче:
 user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
 user_profile["phone"] = "+380991234567"
