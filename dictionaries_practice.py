165d90a7380e1c437fda334f1b2c08a9f969925a
Uliana Hrytsenko <uliana.hrytsenko.25@kse.org.ua>
2026-07-01 16:45:52 +0300

    database processing 2

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -20,6 +20,11 @@ EMPLOYEE_DB = {
 
 print(f"Alices salary: {EMPLOYEE_DB["tech"]["alice"]["salary"]}")
 
+# EMPLOYEE_DB["sales"]["diana"]["salary"] = 3500
+# del EMPLOYEE_DB["tech"]["charlie"]
+# print(EMPLOYEE_DB)
+
+
 
 # Ваш код для завдань нижче:
 
