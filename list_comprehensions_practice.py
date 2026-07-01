7c153f778d88c641b537151eec750faf5047aef4
maxander <andrii.maksymov.25@ske.org.ua>
2026-07-01 15:39:38 +0300

    Revise README with updated links and formatting
    Updated links for materials and improved formatting.

--- list_comprehensions_practice.py
+++ list_comprehensions_practice.py
@@ -3,15 +3,22 @@
 # Використовується для Практичної №2
 # ==========================================
 
-TRANSACTIONS_DB = [
-    {"id": 1, "client": "Alice", "amount": 1200, "category": "Electronics", "status": "success"},
-    {"id": 2, "client": "Bob", "amount": 450, "category": "Groceries", "status": "success"},
-    {"id": 3, "client": "Charlie", "amount": -150, "category": "Refund", "status": "success"},
-    {"id": 4, "client": "Diana", "amount": 55000, "category": "Auto", "status": "pending"},
-    {"id": 5, "client": "Eve", "amount": 800, "category": "Electronics", "status": "failed"},
-    {"id": 6, "client": "Frank", "amount": 200, "category": "Groceries", "status": "success"},
-    {"id": 7, "client": "Grace", "amount": 15000, "category": "Electronics", "status": "success"},
-    {"id": 8, "client": "Hank", "amount": -50, "category": "Refund", "status": "failed"},
+# TRANSACTIONS_DB = [
+#     {"id": 1, "client": "Alice", "amount": 1200, "category": "Electronics", "status": "success"},
+#     {"id": 2, "client": "Bob", "amount": 450, "category": "Groceries", "status": "success"},
+#     {"id": 3, "client": "Charlie", "amount": -150, "category": "Refund", "status": "success"},
+#     {"id": 4, "client": "Diana", "amount": 55000, "category": "Auto", "status": "pending"},
+#     {"id": 5, "client": "Eve", "amount": 800, "category": "Electronics", "status": "failed"},
+#     {"id": 6, "client": "Frank", "amount": 200, "category": "Groceries", "status": "success"},
+#     {"id": 7, "client": "Grace", "amount": 15000, "category": "Electronics", "status": "success"},
+#     {"id": 8, "client": "Hank", "amount": -50, "category": "Refund", "status": "failed"},
 ]
 
 # Ваш код для завдань нижче:
+
+
+
+balances = [5000, -1000, 250, -500]
+
+new_balances = [x if x>0 else x*1.1 for x in balances]
+print(new_balances)
