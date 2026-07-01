5da7c5af572d36c0f4b1e7ab2a1440b1ac75ca48
VVV-Rozvodovskyy <volodymyr.rozvodovskyi.25@kse.org.ua>
2026-07-01 17:14:16 +0300

    fifth dictionary task completed

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -22,9 +22,18 @@ EMPLOYEE_DB = {
 # print(f"Alices salary: {EMPLOYEE_DB['tech']['alice']['salary']}")
 
 
-EMPLOYEE_DB['sales']['diana']['salary'] = 3500
-del EMPLOYEE_DB['tech']['charlie']
-print(EMPLOYEE_DB)
+# EMPLOYEE_DB['sales']['diana']['salary'] = 3500
+# del EMPLOYEE_DB['tech']['charlie']
+# print(EMPLOYEE_DB)
+
+total_salary = 0
+amaunt_p = 0
+for dep_name, people in EMPLOYEE_DB.items():
+    for name, person_data in people.items():
+        total_salary += person_data["salary"]
+        amaunt_p += 1
+print(f"Average salary: {total_salary/amaunt_p}")
+
 
 
 
@@ -34,7 +43,7 @@ print(EMPLOYEE_DB)
 # user_profile.pop("discount")
 # print(user_profile)
 
-grades = {"Alice": 95, "Bob": 80, "Charlie": 75, "Diana": 90}
-z = list(grades.values())
-average = sum(z)/len(z)
-print(f"Average: {average:.2f}")
+# grades = {"Alice": 95, "Bob": 80, "Charlie": 75, "Diana": 90}
+# z = list(grades.values())
+# average = sum(z)/len(z)
+# print(f"Average: {average:.2f}")
