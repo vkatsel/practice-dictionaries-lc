037486490b1015f00c15285af42beab2bb3abb71
Pavlo-Demchuk <pavlo.demchuk.25@kse.org.ua>
2026-07-01 16:04:42 +0300

    Completed second task (dictionaries)

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -20,8 +20,13 @@ EMPLOYEE_DB = {
 
 # Ваш код для завдань нижче:
 
-user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
-user_profile["phone"] = "+380991234567"
-user_profile["is_premium"] = False
-user_profile.pop("discount")
-print(user_profile)
+#user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
+#user_profile["phone"] = "+380991234567"
+#user_profile["is_premium"] = False
+#user_profile.pop("discount")
+#print(user_profile)
+
+grades = {"Alice": 95, "Bob": 80, "Charlie": 75, "Diana": 90}
+pure_grades = list(grades.values())
+average = sum(pure_grades) / len(pure_grades)
+print(f"Average grade: {average:.2f}")
