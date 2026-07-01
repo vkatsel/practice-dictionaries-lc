9cc8277fd5f8f7431be10df68084ad0d8bf7aebb
mariiavasylieva25 <mariia.vasylieva.25@kse.org.ua>
2026-07-01 16:08:26 +0300

    Completed second task (dictionaries)

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -19,12 +19,20 @@ EMPLOYEE_DB = {
 }
 
 # Ваш код для завдань нижче:
-user_profile = {"username": "cinema_fan",
-                "is_premium": True,
-                "discount": 15
-}
-user_profile["phone"] = "+380991234567"
-user_profile["is_premium"] = False
-user_profile.pop("discount")
-user_profile.pop("is_premium")
-print(user_profile)
+# user_profile = {"username": "cinema_fan",
+#                 "is_premium": True,
+#                 "discount": 15
+# }
+# user_profile["phone"] = "+380991234567"
+# user_profile["is_premium"] = False
+# user_profile.pop("discount")
+# user_profile.pop("is_premium")
+# print(user_profile)
+
+
+grades = {"Alice": 95, "Bob": 80, "Charlie": 75, "Diana": 90}
+pure_grades = list(grades.values())
+avarage = sum(pure_grades)/len(pure_grades)
+print(f"Average grade: {avarage:.2f}")
+a = 1
+print(f"{a:06d}")
