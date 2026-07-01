902327481f5aa13edcb7361c82b922df9f273136
maxander <andrii.maksymov.25@ske.org.ua>
2026-07-01 16:05:33 +0300

    completed first task(dictionaries)

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -19,8 +19,13 @@ EMPLOYEE_DB = {
 }
 
 # Ваш код для завдань нижче:
-user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
-user_profile["is_premium"] = False
-user_profile["phone"] = "+380991234567"
-user_profile.pop("discount")
-print(user_profile)
+# user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
+# user_profile["is_premium"] = False
+# user_profile["phone"] = "+380991234567"
+# user_profile.pop("discount")
+# print(user_profile)
+
+grades = {"Alice": 95, "Bob": 80, "Charlie": 75, "Diana": 90}
+grades_naked= list(grades.values())
+av = sum(grades_naked) / len(grades)
+print(av)
