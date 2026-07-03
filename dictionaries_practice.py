552b03ae7ecf03e322bad12a96c7ab018faa5bea
ya-lavreniuk <yana.lavreniuk.25@kse.org.ua>
2026-07-03 15:06:02 +0300

    task 3 completed (dictionaries)

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -36,6 +36,12 @@ user_profile["is_premium"]=False
 user_profile.pop("discount")
 print(user_profile)
 
+expenses = {"Marketing": 5000, "Rent": 2000, "Salaries": 15000}
+total=0
+for ex in expenses.values():
+    total += ex
+print(f"Total expenses: {total}")
+
 # ==========================================
 # БАЗА ТРАНЗАКЦІЙ ТА КЛІЄНТІВ (СПИСОК СЛОВНИКІВ)
 # Використовується для фінальних завдань
