65fedf9b53479f560f4339cfb7b6bcfee9653847
a.buteiko <anton.buteiko@gmail.com>
2026-07-03 11:56:43 +0300

    dict

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -53,9 +53,7 @@ TRANSACTIONS_DB = [
 # print(f"Updated product: {product}")
 
 
-user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
-user_profile["phone"] = "+380991234567"
-user_profile["is_premium"] = False
-user_profile.pop("discount")
 
-print(user_profile)
+# expenses = {"Marketing": 5000, "Rent": 2000, "Salaries": 15000}
+# print(sum(expenses.values()))
+
