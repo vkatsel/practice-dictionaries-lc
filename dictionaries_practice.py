a120d8fee90e34eb747c9f6f75bdac531633794f
maxoderiy <maksym.oderii.25@kse.org.ua>
2026-07-03 15:35:16 +0300

    task 7 (2 part)

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -121,22 +121,46 @@ EMPLOYEE_DB = {
 
 
 
-EMPLOYEE_DB = {
-    "tech": {
-        "alice": {"role": "Data Scientist", "salary": 4500, "status": "active"},
-        "bob": {"role": "Backend Engineer", "salary": 3800, "status": "active"},
-        "charlie": {"role": "DevOps", "salary": 4000, "status": "on_leave"}
-    },
-    "sales": {
-        "diana": {"role": "Sales Manager", "salary": 3000, "status": "active"},
-        "eve": {"role": "Account Executive", "salary": 3200, "status": "active"}
-    },
-    "hr": {
-        "frank": {"role": "HR Specialist", "salary": 2500, "status": "active"}
-    }
-}
+# EMPLOYEE_DB = {
+#     "tech": {
+#         "alice": {"role": "Data Scientist", "salary": 4500, "status": "active"},
+#         "bob": {"role": "Backend Engineer", "salary": 3800, "status": "active"},
+#         "charlie": {"role": "DevOps", "salary": 4000, "status": "on_leave"}
+#     },
+#     "sales": {
+#         "diana": {"role": "Sales Manager", "salary": 3000, "status": "active"},
+#         "eve": {"role": "Account Executive", "salary": 3200, "status": "active"}
+#     },
+#     "hr": {
+#         "frank": {"role": "HR Specialist", "salary": 2500, "status": "active"}
+#     }
+# }
+#
+# EMPLOYEE_DB["sales"]["diana"]["salary"] = 3500
+# EMPLOYEE_DB["tech"].pop("charlie")
+#
+# print(EMPLOYEE_DB)
 
-EMPLOYEE_DB["sales"]["diana"]["salary"] = 3500
-EMPLOYEE_DB["tech"].pop("charlie")
 
-print(EMPLOYEE_DB)
+
+TRANSACTIONS_DB = [
+    {"id": 1, "client": "Alice", "amount": 1200, "category": "Electronics", "status": "success"},
+    {"id": 2, "client": "Bob", "amount": 450, "category": "Groceries", "status": "success"},
+    {"id": 3, "client": "Charlie", "amount": -150, "category": "Refund", "status": "success"},
+    {"id": 4, "client": "Diana", "amount": 55000, "category": "Auto", "status": "pending"},
+    {"id": 5, "client": "Eve", "amount": 800, "category": "Electronics", "status": "failed"},
+    {"id": 6, "client": "Frank", "amount": 200, "category": "Groceries", "status": "success"},
+    {"id": 7, "client": "Grace", "amount": 15000, "category": "Electronics", "status": "success"},
+    {"id": 8, "client": "Hank", "amount": -50, "category": "Refund", "status": "failed"},
+]
+
+active_clients = [transaction["client"] for transaction in TRANSACTIONS_DB if transaction["status"] == "success"]
+print(active_clients)
+
+high_value_transactions = [transaction for transaction in TRANSACTIONS_DB if transaction["amount"] > 10000]
+
+print(high_value_transactions)
+
+refunds = [transaction ["amount"] for transaction in TRANSACTIONS_DB if transaction["category"] == "Refund"]
+
+print(f"refunds: {refunds}")
