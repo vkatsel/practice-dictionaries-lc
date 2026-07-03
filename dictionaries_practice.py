99c7dd55a4109185d098acc8931bc63478efd531
Pavlo-Demchuk <pavlo.demchuk.25@kse.org.ua>
2026-07-03 15:39:28 +0300

    7 task

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -67,3 +67,28 @@ for departament in EMPLOYEE_DB.values():
         employee_count += 1
 employee_average = salary_total / employee_count
 print(f"Average salary: {employee_average}")
+
+
+
+
+TRANSACTIONS_DB = [
+    {"id": 1, "client": "Alice", "amount": 1200, "category": "Electronics", "status": "success"},
+    {"id": 2, "client": "Bob", "amount": 450, "category": "Groceries", "status": "success"},
+    {"id": 3, "client": "Charlie", "amount": -150, "category": "Refund", "status": "success"},
+    {"id": 4, "client": "Diana", "amount": 55000, "category": "Auto", "status": "pending"},
+    {"id": 5, "client": "Eve", "amount": 800, "category": "Electronics", "status": "failed"},
+    {"id": 6, "client": "Frank", "amount": 200, "category": "Groceries", "status": "success"},
+    {"id": 7, "client": "Grace", "amount": 15000, "category": "Electronics", "status": "success"},
+    {"id": 8, "client": "Hank", "amount": -50, "category": "Refund", "status": "failed"},
+]
+
+active_clients = [transaction["client"] for transaction in TRANSACTIONS_DB if transaction["status"] == "success"]
+print(active_clients)
+
+high_value_transactions = [transaction for transaction in TRANSACTIONS_DB if transaction["amount"] > 10000]
+
+print(high_value_transactions)
+
+refunds = [transaction ["amount"] for transaction in TRANSACTIONS_DB if transaction["category"] == "Refund"]
+
+print(f"refunds: {refunds}")
