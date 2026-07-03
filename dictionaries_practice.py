5038471c25df5a5ad4ff0318dd8e4cf268782e88
Viktoriia <viktoriia.usenko.25@kse.org.ua>
2026-07-03 11:50:33 +0300

    guided practise

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -37,11 +37,24 @@ TRANSACTIONS_DB = [
 ]
 
 
-menu = {"Latte": 60, "Americano": 40}
-print(f"Americano: {menu['Americano']}")
+# menu = {"Latte": 60, "Americano": 40}
+# print(f"Americano: {menu['Americano']}")
+#
+# menu["Latte"]=70
+# menu["Flat-white"]=65
+#
+# print(f"Updated dict: {menu}")
+# print(f"Cappuchino: {menu.get('Cappuchino', "Undefinded")}")
 
-menu["Latte"]=70
-menu["Flat-white"]=65
 
-print(f"Updated dict: {menu}")
-print(f"Cappuchino: {menu.get('Cappuchino', "Undefinded")}")
+# product = {"name": "Laptop", "price": 1000, "stock": 15}
+# product["price"] *= 1.1
+# product["stock"] -= 1
+# print(f"Updated product: {product}")
+
+
+user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
+user_profile['phone'] = "+380991234567"
+user_profile["is_premium"] = False
+removed = user_profile.pop("discount")
+print(f"Updated profile is: {user_profile}")
