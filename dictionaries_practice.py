b21894c11eddbf2dde4c80d3f399d8d6c11bf0dc
maxoderiy <maksym.oderii.25@kse.org.ua>
2026-07-03 15:05:35 +0300

    task 3 (2 part)

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -62,13 +62,22 @@ TRANSACTIONS_DB = [
 
 
 
-user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
+# user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
+#
+# user_profile["is_premium"] = False
+#
+# user_profile["phone"] = "+380991234567"
+#
+# user_profile.pop("discount")
+# print(user_profile)
 
-user_profile["is_premium"] = False
 
-user_profile["phone"] = "+380991234567"
 
-user_profile.pop("discount")
-print(user_profile)
 
+expenses = {"Marketing": 5000, "Rent": 2000, "Salaries": 15000}
+
+suma= 0
+for key , value in expenses.items():
+    suma += value
+print(suma)
 
