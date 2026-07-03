df1ac4695d5330e470b992163696ef917e8db058
Tesoley <01dashakobylianska@gmail.com>
2026-07-03 12:52:54 +0300

    generated json is used👦🏽🫃🏽

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -3,6 +3,11 @@
 # Використовується для Практичної №1
 # ==========================================
 
+# for department, employee in EMPLOYEE_DB.
+# ==========================================
+# БАЗА ТРАНЗАКЦІЙ ТА КЛІЄНТІВ (СПИСОК СЛОВНИКІВ)
+# Використовується для фінальних завдань
+# ==========================================
 EMPLOYEE_DB = {
     "tech": {
         "alice": {"role": "Data Scientist", "salary": 4500, "status": "active"},
@@ -19,29 +24,32 @@ EMPLOYEE_DB = {
 }
 
 # Ваш код для завдань нижче:
-alice = EMPLOYEE_DB["tech"]["alice"]["salary"]
-print(f"Alice salary is: {alice}")
+# alice = EMPLOYEE_DB["tech"]["alice"]["salary"]
+# print(f"Alice salary is: {alice}")
+#
+# total_salary = 0
+# total_employees = 0
+# for key, depar in EMPLOYEE_DB.items():
+#     total_employees += len(depar)
+#     depar_sal = 0
+#
+#     for employee in depar:
+#         total_salary += depar[employee]["salary"]
+#         depar_sal += depar[employee]["salary"]
+#
+#     print(f"Salary for {key}: {depar_sal / len(depar)}")
+#
+# print(f"Total salary for al departs: {total_salary / total_employees}")
+#
+#
+#
+# # ```````````````============Live Coding 3: Company Mini-DB=============```````````````
+# EMPLOYEE_DB["sales"]["diana"]["salary"] = 3500
+# fired = EMPLOYEE_DB["tech"].pop("charlie")
+# print(EMPLOYEE_DB)
 
-total_salary = 0
-total_employees = 0
-for key, depar in EMPLOYEE_DB.items():
-    total_employees += len(depar)
-    depar_sal = 0
 
-    for employee in depar:
-        total_salary += depar[employee]["salary"]
-        depar_sal += depar[employee]["salary"]
-
-    print(f"Salary for {key}: {depar_sal / len(depar)}")
-
-print(f"Total salary for al departs: {total_salary / total_employees}")
-
-# for department, employee in EMPLOYEE_DB.
-# ==========================================
-# БАЗА ТРАНЗАКЦІЙ ТА КЛІЄНТІВ (СПИСОК СЛОВНИКІВ)
-# Використовується для фінальних завдань
-# ==========================================
-
+# ```````````````============  Capstone: Analytical dashboard  =============```````````````
 TRANSACTIONS_DB = [
     {"id": 1, "client": "Alice", "amount": 1200, "category": "Electronics", "status": "success"},
     {"id": 2, "client": "Bob", "amount": 450, "category": "Groceries", "status": "success"},
@@ -53,7 +61,15 @@ TRANSACTIONS_DB = [
     {"id": 8, "client": "Hank", "amount": -50, "category": "Refund", "status": "failed"},
 ]
 
+success_clients = [t["client"] for t in TRANSACTIONS_DB
+                   if t["status"] == "success"]
+print(success_clients)
 
+high_value_t = [t for t in TRANSACTIONS_DB if t["amount"] >= 10000]
+print(high_value_t)
+
+refund = [t["amount"] for t in TRANSACTIONS_DB if t["category"] == "Refund"]
+print(refund)
 # menu = {"Latte": 60, "Americano": 40}
 # print(f"Americano: {menu["Americano"]}")
 #
@@ -73,22 +89,113 @@ TRANSACTIONS_DB = [
 # removed = user_profile.pop("discount")
 # print(f"Updated profile is: {user_profile}", removed)
 
-expenses = {"Marketing": 5000, "Rent": 2000, "Salaries": 15000}
-print(sum(list(expenses.values())))
+# expenses = {"Marketing": 5000, "Rent": 2000, "Salaries": 15000}
+# print(sum(list(expenses.values())))
+#
+# suma = 0
+# for expense in expenses.values():
+#     suma += expense
+# print(f"The sum is: {suma}")
+#
+# # ```````````````============Guided Practice: Аналіз оцінок=============```````````````
+#
+# grades = {"Alice": 95, "Bob": 80, "Charlie": 75, "Diana": 90}
+# total = 0
+# # print(sum(list(grades.values())))
+# for grade in grades.values():
+#     total += grade
+#
+# average = total/len(grades)
+# print(average)
+#
+
+
+
+
+
+experiment_json = [{
+  "transactionId": "TXN-8749302-A",
+  "timestamp": "2026-07-03T12:39:00Z",
+  "type": "transfer",
+  "status": "completed",
+  "source": {
+    "accountId": "ACC-99482-S",
+    "accountHolder": "John Doe",
+    "bank": "Global Trust Bank"
+  },
+  "destination": {
+    "accountId": "ACC-11029-D",
+    "accountHolder": "Jane Smith",
+    "bank": "First National Bank"
+  },
+  "amount": {
+    "value": 1000.00,
+    "currency": "USD"
+  },
+  "metadata": {
+    "description": "Monthly rent payment",
+    "referenceCode": "RENT-JUL-2026",
+    "ipAddress": "192.168.1.15"
+  }
+},
+    {
+        "transactionId": "TXN-8749302-A",
+        "timestamp": "2026-07-03T12:39:00Z",
+        "type": "transfer",
+        "status": "completed",
+        "source": {
+            "accountId": "ACC-99482-S",
+            "accountHolder": "John Doe",
+            "bank": "Global Trust Bank"
+        },
+        "destination": {
+            "accountId": "ACC-11029-D",
+            "accountHolder": "Jane Smith",
+            "bank": "First National Bank"
+        },
+        "amount": {
+            "value": 2000.00,
+            "currency": "USD"
+        },
+        "metadata": {
+            "description": "Monthly rent payment",
+            "referenceCode": "RENT-JUL-2026",
+            "ipAddress": "192.168.1.15"
+        }
+    }
+,
+    {
+        "transactionId": "TXN-8749302-A",
+        "timestamp": "2026-07-03T12:39:00Z",
+        "type": "transfer",
+        "status": "completed",
+        "source": {
+            "accountId": "ACC-99482-S",
+            "accountHolder": "John Doe",
+            "bank": "Global Trust Bank"
+        },
+        "destination": {
+            "accountId": "ACC-11029-D",
+            "accountHolder": "Jane Smith",
+            "bank": "First National Bank"
+        },
+        "amount": {
+            "value": 1500.00,
+            "currency": "USD"
+        },
+        "metadata": {
+            "description": "Monthly rent payment",
+            "referenceCode": "RENT-JUL-2026",
+            "ipAddress": "192.168.1.15"
+        }
+    }
+]
+
+print(experiment_json[0]["source"]["accountId"])
+print(experiment_json[1]["amount"]["currency"])
 
 suma = 0
-for expense in expenses.values():
-    suma += expense
-print(f"The sum is: {suma}")
+for t in experiment_json:
+    suma = t["amount"]["value"]
 
-# ```````````````============Guided Practice: Аналіз оцінок=============```````````````
-
-grades = {"Alice": 95, "Bob": 80, "Charlie": 75, "Diana": 90}
-total = 0
-# print(sum(list(grades.values())))
-for grade in grades.values():
-    total += grade
-
-average = total/len(grades)
-print(average)
-
+print(suma / len(experiment_json))
