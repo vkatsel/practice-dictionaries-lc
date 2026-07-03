b6691ce8bedef421946aac2f108ae47aa9e2a1ca
Mariia Zahorui <mariia.zahorui.25@kse.org.ua>
2026-07-03 11:40:48 +0300

    2 practice

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -35,3 +35,10 @@ TRANSACTIONS_DB = [
     {"id": 7, "client": "Grace", "amount": 15000, "category": "Electronics", "status": "success"},
     {"id": 8, "client": "Hank", "amount": -50, "category": "Refund", "status": "failed"},
 ]
+
+menu = {"Latte": 60, "Americano": 40}
+print(f"Americano: {menu['Americano']}")
+
+menu["Latte"] = 70
+menu["Flat White"]= 65
+print(f"Updated dict:{menu}")
