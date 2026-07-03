4b76eea95d720efd4dbe2a23db4483db961340d2
K-Iovenko <kyrylo.iovenko.25@kse.org.ua>
2026-07-03 14:33:20 +0300

    guided practice 1 dict

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -52,3 +52,9 @@ product = {"name": "Laptop", "price": 1000, "stock": 15}
 product['stock'] -= 1
 product['price'] *= 1.1
 print(product)
+
+user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
+user_profile["phone"] = "+380991234567"
+user_profile["is_premium"] = False
+user_profile.pop('discount')
+print(user_profile)
