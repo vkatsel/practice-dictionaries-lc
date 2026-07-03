726dd8d0a6c62b7b6f513a2a30e0617296098f84
t-ryzhkova <tetiana.ryzhkova.25@kse.org.ua>
2026-07-03 14:12:42 +0300

    completed task_5 & task_6 (lc)

--- list_comprehensions_practice.py
+++ list_comprehensions_practice.py
@@ -46,3 +46,28 @@ vip_transactions = [trans for trans in transactions if trans>10000]
 print(f"Vip transactions: ", end="")
 print(*vip_transactions)
 
+# №5
+SERVER_LOGS = [
+    "INFO: User logged in",
+    "ERROR: Connection timeout",
+    "DEBUG: Query executed",
+    "ERROR: Database locked",
+    "WARNING: High memory usage",
+    "ERROR: File not found",
+    "INFO: User logged out"
+]
+no_error = [text[7:] for text in SERVER_LOGS if text.startswith("ERROR")]
+print(no_error)
+
+# №6
+RAW_PRICES = [
+    "$10.50",
+    "€20.00",
+    "$15.99",
+    "invalid",
+    "$5.00",
+    "$120.00",
+    "N/A"
+]
+valid_prices = [float(price[1:]) for price in RAW_PRICES if price.startswith("$") or price.startswith("€")]
+print(valid_prices)
