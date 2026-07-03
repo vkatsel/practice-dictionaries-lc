d82c45da31ab7e7fcd9adb72cc9e6fcfe97be386
Ivan Turynsky <ivan.turinskyi.25@kse.org.ua>
2026-07-03 14:02:44 +0300

    solved bugs in 3 tasks

--- list_comprehensions_practice.py
+++ list_comprehensions_practice.py
@@ -30,7 +30,6 @@ net_salaries=[sal*0.8 for sal in gross_salaries]
 print(net_salaries)
 
 raw_names = ["aLice", "BOB", "charlie", "dIAnA"]
-
 clean_names=[name.title() for name in raw_names]
 print(clean_names)
 
@@ -39,5 +38,15 @@ with_fine = [bal*1.1 if bal<0 else bal for bal in balances]
 print(with_fine)
 
 transactions = [1200, 15000, 450, 55000, 800]
-vip_trans=[trans for trans in transactions if trans >=10000]
-print(vip_trans)
+vip_trans=[trans for trans in transactions if trans >10000]
+print("VIP transactions", *vip_trans)
+
+numbers = [1, 2, 3]
+doubled = [x*2 for x in numbers]
+
+amounts = [50, 120, 30]
+high_amounts = [a for a in amounts if a > 100]
+print(high_amounts)
+
+data = [10, -5, 20, -1]
+clean_data = [0 if x < 0 else x for x in data]
