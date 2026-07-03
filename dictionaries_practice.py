57593b7d613719dc1a9d0392e250818838362b64
olhac <olha.cherniavska.25@kse.org.ua>
2026-07-03 14:34:33 +0300

    completed task 2

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -34,7 +34,12 @@ product["stock"] -=1
 product["price"] *=1.1
 print(product)
 
-
+#2
+user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
+user_profile["phone"] = "+380991234567"
+user_profile["is_premium"] = False
+discount = user_profile.pop("discount")
+print(user_profile)
 
 
 
