6aa29429c93cce239acfc57c6ba4868e1aa576f0
ceoet <nazar.vatsyk.25@kse.otg.ua>
2026-07-03 11:28:51 +0300

    Live coding 2

--- list_comprehensions_practice.py
+++ list_comprehensions_practice.py
@@ -18,9 +18,11 @@ TRANSACTIONS_DB = [
 # gross_salaries = [15000, 22000, 18500, 31000]
 # net_salaries = [salary*0.8 for salary in gross_salaries]
 # print(net_salaries)
-raw_names = ["aLice", "BOB", "charlie", "dIAnA"]
-clean_names = [name.title() for name in raw_names]
-print(clean_names)
+# raw_names = ["aLice", "BOB", "charlie", "dIAnA"]
+# clean_names = [name.title() for name in raw_names]
+# print(clean_names)
+# balances = [5000, -1000, 250, -500]
+# print([b*1.1 if b<0 else b for b in balances])
 # Refunds = [t["amount"] for t in TRANSACTIONS_DB if t["category"]== "Refund"]
 # print(Refunds)
 # Total = sum([t["amount"] for t in TRANSACTIONS_DB if t["status"] == "success"])
