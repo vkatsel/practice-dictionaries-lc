ba3641eb2b7a4f3faded5e71c130ea4c88952a84
Viktoriia <viktoriia.usenko.25@kse.org.ua>
2026-07-03 11:08:00 +0300

    live coding 3 done

--- list_comprehensions_practice.py
+++ list_comprehensions_practice.py
@@ -40,6 +40,37 @@ RAW_PRICES = [
 # print(f"Net salaries: {net_salaries}")
 
 
-raw_names = ["aLice", "BOB", "charlie", "dIAnA"]
-clean_names=[name.title() for name in raw_names]
-print(clean_names)
+# raw_names = ["aLice", "BOB", "charlie", "dIAnA"]
+# clean_names=[name.title() for name in raw_names]
+# print(clean_names)
+
+
+# balances = [5000, -10000, 250, -500]
+# balances_new = [balance + (balance*0.1) if balance <0 else balance
+#                 for balance in balances]
+# print(balances_new)
+#
+# status_bank = "good" if sum(balances)>3000 else "baaad"
+# print(f"Bank bancrupcy status: {status_bank}")
+
+
+# transactions = [1200, 15000, 450, 55000, 800]
+# vip_transactions = [transaction*2 if transaction%2==0 else transaction/2 for transaction in transactions if transaction>10000]
+# print(vip_transactions)
+
+
+# cleaned_messages = [ log.replace("ERROR:", " ").strip  for log in SERVER_LOGS if log.startswith("ERROR")]
+# print(cleaned_messages)
+
+
+RAW_PRICES = [
+    "$10.50",
+    "€20.00",
+    "$15.99",
+    "invalid",
+    "$5.00",
+    "$120.00",
+    "N/A"
+]
+only_price = [ float([p:1])  for p in RAW_PRICES if p.startswith("$")]
+print(only_price)
