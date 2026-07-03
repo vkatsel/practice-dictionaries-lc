bcbc6edc5d93daed431e7a5fbce21724dbec5bff
s-korolova-kazanska <sofiia.korolova-kazanska.25@kse.org.ua>
2026-07-03 10:50:07 +0300

    add live coding 2 and guided practise 2

--- list_comprehensions_practice.py
+++ list_comprehensions_practice.py
@@ -25,22 +25,35 @@ RAW_PRICES = [
 
 # Ваш код для завдань нижче:
 
-numbers = [1, 2, 3, 4, 5]
-results = []
+# numbers = [1, 2, 3, 4, 5]
+# results = []
+#
+# for num in numbers:
+#     squared = num ** 2
+#     results.append(squared)
+#
+# print(results)
+#
+# results_2 = [num ** 2 for num in numbers]
+# print(results_2)
+#
+# gross_salaries = [15000, 22000, 18500, 31000]
+# net_salaries = [salary - (salary * 0.2) for salary in gross_salaries]
+# print(net_salaries)
+#
+# raw_names = ["aLice", "BOB", "charlie", "dIAnA"]
+# clean_names = [name.title() for name in raw_names]
+# print(clean_names)
 
-for num in numbers:
-    squared = num ** 2
-    results.append(squared)
+balances = [5000, -1000, 250, -500]
+balances_new = [balance + (balance * 0.1) if balance < 0 else balance
+                for balance in balances]
+print(balances_new)
 
-print(results)
+status_bank = "good" if sum(balances) > 3000 else "bad"
+print(f"Bank bancrupcy status: {status_bank}")
 
-results_2 = [num ** 2 for num in numbers]
-print(results_2)
 
-gross_salaries = [15000, 22000, 18500, 31000]
-net_salaries = [salary - (salary * 0.2) for salary in gross_salaries]
-print(net_salaries)
-
-raw_names = ["aLice", "BOB", "charlie", "dIAnA"]
-clean_names = [name.title() for name in raw_names]
-print(clean_names)
+transactions = [1200, 15000, 450, 55000, 800]
+vip_transactions = [suma for suma in transactions if suma > 10000]
+print(vip_transactions)
