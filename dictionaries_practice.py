5f1871778cb8715801ab65771c034e1543a806f6
arseniykrasutskyi <arsenii.krasutskyi.25@kse.org.ua>
2026-07-03 12:02:04 +0300

    grades

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -47,8 +47,16 @@ TRANSACTIONS_DB = [
 # product["stock"] -= 1
 # print(f"Update product: {product}")
 
-user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
-user_profile["is_premium"] = False
-user_profile["phone"] = "+380991234567"
-user_profile.pop("discount")
-print(f"Update profile: {user_profile}")
+# user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
+# user_profile["is_premium"] = False
+# user_profile["phone"] = "+380991234567"
+# user_profile.pop("discount")
+# print(f"Update profile: {user_profile}")
+# expenses = {"Marketing": 5000, "Rent": 2000, "Salaries": 15000}
+# suma = 0
+# for expense in expenses.values():
+#     suma += expense
+# print(f"suma: {suma}")
+grades = {"Alice": 95, "Bob": 80, "Charlie": 75, "Diana": 90}
+average = sum(grades.values())/len(grades)
+print(f"Average grade: {average}")
