c4ad7df84aeb898a22986cfda2e832cfa1068f74
damirmac <damyr.viietskyi.25@kse.org.ua>
2026-07-03 11:31:23 +0300

    Practice 2 1

--- dictionaries_practice.py
+++ dictionaries_practice.py
@@ -3,38 +3,38 @@
 # Використовується для Практичної №1
 # ==========================================
 
-EMPLOYEE_DB = {
-    "tech": {
-        "alice": {"role": "Data Scientist", "salary": 4500, "status": "active"},
-        "bob": {"role": "Backend Engineer", "salary": 3800, "status": "active"},
-        "charlie": {"role": "DevOps", "salary": 4000, "status": "on_leave"}
-    },
-    "sales": {
-        "diana": {"role": "Sales Manager", "salary": 3000, "status": "active"},
-        "eve": {"role": "Account Executive", "salary": 3200, "status": "active"}
-    },
-    "hr": {
-        "frank": {"role": "HR Specialist", "salary": 2500, "status": "active"}
-    }
-}
-
-# Ваш код для завдань нижче:
-
-# ==========================================
-# БАЗА ТРАНЗАКЦІЙ ТА КЛІЄНТІВ (СПИСОК СЛОВНИКІВ)
-# Використовується для фінальних завдань
-# ==========================================
-
-TRANSACTIONS_DB = [
-    {"id": 1, "client": "Alice", "amount": 1200, "category": "Electronics", "status": "success"},
-    {"id": 2, "client": "Bob", "amount": 450, "category": "Groceries", "status": "success"},
-    {"id": 3, "client": "Charlie", "amount": -150, "category": "Refund", "status": "success"},
-    {"id": 4, "client": "Diana", "amount": 55000, "category": "Auto", "status": "pending"},
-    {"id": 5, "client": "Eve", "amount": 800, "category": "Electronics", "status": "failed"},
-    {"id": 6, "client": "Frank", "amount": 200, "category": "Groceries", "status": "success"},
-    {"id": 7, "client": "Grace", "amount": 15000, "category": "Electronics", "status": "success"},
-    {"id": 8, "client": "Hank", "amount": -50, "category": "Refund", "status": "failed"},
-]
+# EMPLOYEE_DB = {
+#     "tech": {
+#         "alice": {"role": "Data Scientist", "salary": 4500, "status": "active"},
+#         "bob": {"role": "Backend Engineer", "salary": 3800, "status": "active"},
+#         "charlie": {"role": "DevOps", "salary": 4000, "status": "on_leave"}
+#     },
+#     "sales": {
+#         "diana": {"role": "Sales Manager", "salary": 3000, "status": "active"},
+#         "eve": {"role": "Account Executive", "salary": 3200, "status": "active"}
+#     },
+#     "hr": {
+#         "frank": {"role": "HR Specialist", "salary": 2500, "status": "active"}
+#     }
+# }
+#
+# # Ваш код для завдань нижче:
+#
+# # ==========================================
+# # БАЗА ТРАНЗАКЦІЙ ТА КЛІЄНТІВ (СПИСОК СЛОВНИКІВ)
+# # Використовується для фінальних завдань
+# # ==========================================
+#
+# TRANSACTIONS_DB = [
+#     {"id": 1, "client": "Alice", "amount": 1200, "category": "Electronics", "status": "success"},
+#     {"id": 2, "client": "Bob", "amount": 450, "category": "Groceries", "status": "success"},
+#     {"id": 3, "client": "Charlie", "amount": -150, "category": "Refund", "status": "success"},
+#     {"id": 4, "client": "Diana", "amount": 55000, "category": "Auto", "status": "pending"},
+#     {"id": 5, "client": "Eve", "amount": 800, "category": "Electronics", "status": "failed"},
+#     {"id": 6, "client": "Frank", "amount": 200, "category": "Groceries", "status": "success"},
+#     {"id": 7, "client": "Grace", "amount": 15000, "category": "Electronics", "status": "success"},
+#     {"id": 8, "client": "Hank", "amount": -50, "category": "Refund", "status": "failed"},
+# ]
 
 # gross_salaries = [15000, 22000, 18500, 31000]
 # net_salaries = [i * 0.8 for i in gross_salaries]
@@ -55,3 +55,10 @@ TRANSACTIONS_DB = [
 # transactions = [1200, 15000, 450, 55000, 800]
 # vip_trancations = [ i for i in transactions if i > 10000]
 # print(vip_trancations)
+
+menu = {"Latte": 60, "Americano": 40}
+print(f"Americano: {menu['Americano']}")
+
+menu["Latte"] = 70
+menu["Flat White"] = 65
+
