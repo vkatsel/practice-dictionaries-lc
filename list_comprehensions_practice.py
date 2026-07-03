1dbecae913302d6c174d40d25c121f05f1920505
sorocinskakaterina <kateryna.sorochynska.25@kse.org.ua>
2026-07-03 10:31:01 +0300

    first comit

--- list_comprehensions_practice.py
+++ list_comprehensions_practice.py
@@ -1,26 +1,45 @@
-# ==========================================
-# ДАНІ ДЛЯ ОБРОБКИ (СПИСКИ РЯДКІВ)
-# Використовується для Практичної №1
-# ==========================================
+# # ==========================================
+# # ДАНІ ДЛЯ ОБРОБКИ (СПИСКИ РЯДКІВ)
+# # Використовується для Практичної №1
+# # ==========================================
+#
+# SERVER_LOGS = [
+#     "INFO: User logged in",
+#     "ERROR: Connection timeout",
+#     "DEBUG: Query executed",
+#     "ERROR: Database locked",
+#     "WARNING: High memory usage",
+#     "ERROR: File not found",
+#     "INFO: User logged out"
+# ]
+#
+# RAW_PRICES = [
+#     "$10.50",
+#     "€20.00",
+#     "$15.99",
+#     "invalid",
+#     "$5.00",
+#     "$120.00",
+#     "N/A"
+# # ]
+# #
+# # # Ваш код для завдань нижче:
+# #
+# numbers = [1, 2, 3, 4, 5]
+# results = []
+#
+# for num in numbers:
+#     squared = num ** 2
+#     results.append(squared)
+#
+# result2 = [num ** 2 for num in numbers]
+# print(results)
+#
+#
+# gross_salaries = [15000, 22000, 18500, 31000]
+# net_salaries = [salary - salary* 0.2 for salary in gross_salaries]
+# print( net_salaries )
 
-SERVER_LOGS = [
-    "INFO: User logged in",
-    "ERROR: Connection timeout",
-    "DEBUG: Query executed",
-    "ERROR: Database locked",
-    "WARNING: High memory usage",
-    "ERROR: File not found",
-    "INFO: User logged out"
-]
-
-RAW_PRICES = [
-    "$10.50",
-    "€20.00",
-    "$15.99",
-    "invalid",
-    "$5.00",
-    "$120.00",
-    "N/A"
-]
-
-# Ваш код для завдань нижче:
+raw_names = ["aLice", "BOB", "charlie", "dIAnA"]
+clean_names = [names.title() for names in raw_names]
+print(clean_names)
