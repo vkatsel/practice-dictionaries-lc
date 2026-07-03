40a979f648b6626772bfad57c8463bc2faa0e6dc
katsolovei <kateryna.solovei.25@kse.org.ua>
2026-07-03 14:11:28 +0300

    task 5 & 6 (lc)

--- list_comprehensions_practice.py
+++ list_comprehensions_practice.py
@@ -3,25 +3,8 @@
 # Використовується для Практичної №1
 # ==========================================
 
-SERVER_LOGS = [
-    "INFO: User logged in",
-    "ERROR: Connection timeout",
-    "DEBUG: Query executed",
-    "ERROR: Database locked",
-    "WARNING: High memory usage",
-    "ERROR: File not found",
-    "INFO: User logged out"
-]
 
-RAW_PRICES = [
-    "$10.50",
-    "€20.00",
-    "$15.99",
-    "invalid",
-    "$5.00",
-    "$120.00",
-    "N/A"
-]
+
 
 # Ваш код для завдань нижче:
 gross_salaries = [15000, 22000, 18500, 31000]
@@ -43,3 +26,31 @@ print(new_balances)
 transactions = [1200, 15000, 450, 55000, 800]
 vip_transactions = [trans for trans in transactions if trans > 10000]
 print(vip_transactions)
+
+
+
+SERVER_LOGS = [
+    "INFO: User logged in",
+    "ERROR: Connection timeout",
+    "DEBUG: Query executed",
+    "ERROR: Database locked",
+    "WARNING: High memory usage",
+    "ERROR: File not found",
+    "INFO: User logged out"
+]
+
+error_logs = [log.replace("ERROR: ", "") for log in SERVER_LOGS if log.startswith("ERROR:")]
+print(error_logs)
+
+
+RAW_PRICES = [
+    "$10.50",
+    "€20.00",
+    "$15.99",
+    "invalid",
+    "$5.00",
+    "$120.00",
+    "N/A"
+]
+clean_prices = [float(price.replace("$", "")) for price in RAW_PRICES if price.startswith("$")]
+print(clean_prices)
