792848736123feec0f073ca28cc7260b21cd4341
sadovnikov-m <mykhailo.sadovnikov.25@kse.org.ua>
2026-07-03 10:30:51 +0300

    adding warm_up, live_coding_1 and guided_practice_1

--- list_comprehensions_practice.py
+++ list_comprehensions_practice.py
@@ -24,3 +24,15 @@ RAW_PRICES = [
 ]
 
 # Ваш код для завдань нижче:
+
+# numbers = [1, 2, 3, 4, 5]
+# results = [num**2 for num in numbers]
+# print(results)
+
+# gross_salaries = [15000, 22000, 18500, 31000]
+# net_salaries=[salary-(salary*0.2) for salary in gross_salaries]
+# print(f"Net salaries: {net_salaries}")
+
+raw_names = ["aLice", "BOB", "charlie", "dIAnA"]
+clean_names=[name.title() for name in raw_names]
+print(clean_names)
