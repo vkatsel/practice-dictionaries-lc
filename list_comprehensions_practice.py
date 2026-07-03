033fe35252b65bfb8d7538cc6b33f5b3a09bb1e1
syurkiv <sofiia.yurkiv.25@kse.org.ua>
2026-07-03 14:34:42 +0300

    task 2 (2para)

--- list_comprehensions_practice.py
+++ list_comprehensions_practice.py
@@ -60,3 +60,9 @@ product["stock"] -=1
 product["price"] *= 1.1
 print(product)
 
+
+user_profile = {"username": "cinema_fan", "is_premium": True, "discount": 15}
+user_profile["phone"] = "+380991234567"
+user_profile["is_premium"] = False
+user_profile.pop("discount")
+print(user_profile)
